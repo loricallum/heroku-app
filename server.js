a5435ab0e34ce6dd5104aa973e41dca78f17aa42
LLewis <lorimlewis@gmail.com>
2019-04-10 14:34:59 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const app = express();
 
+const routes = require('./routes');
+
 const PORT = process.env.PORT || 5000;
 
-//configure body parser for AJAX requests
+// require db connection
+require('./models');
 
+// configure body parser for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//routes
+// add this line
+app.use(express.static('client/build'));
 
-app.get('/', (req, res) => {
-    res.send('Hi there from MERN');
+app.use(routes);
 
-});
-
-//Bootstrap server
-
+// Bootstrap server
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}.`);
+	console.log(`Server listening on port ${PORT}.`);
 });
